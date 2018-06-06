56bac9abbd2b1b30e16c3c1a23a1d80caedd6eb1
msharafjr <msharaf_m10@yahoo.com>
2018-06-06 05:48:13 +0200

    try commit after modifing the file

--- check-verified-signature.js
+++ check-verified-signature.js
@@ -2,3 +2,4 @@ console.log( 'verified signature' );
 console.log( 'verify public gpg key on github' );
 console.log( 'try commit whithout enter passphrase' );
 console.log( 'change ".gpg-agent.conf" to "gpg-agent.conf"' );
+console.log( 'try to commit after modifing the file' );
